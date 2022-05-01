c079a209c9a1b9ea29936da3f46d960b8974bd2e
Nivas Muthu M G <nivasmuthu452@gmail.com>
2022-05-01 11:51:21 +0530

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/thats_a_face/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.thats_a_face
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeView(),
+    );
+  }
+}
+
+class HomeView extends StatelessWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ElevatedButton(
+          onPressed: () {},
+          child: Text("Computer Vision"),
+        ),
+      ],
+    );
+  }
+}
